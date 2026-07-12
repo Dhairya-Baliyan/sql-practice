f64c8849341e82b185d5582e10188d40035791e6
Dhairya <dhairyabaliyan0812@gmail.com>
2026-07-12 21:14:26 +0530

    Finished basic select questions.

--- HackerRank/basic-select.sql
+++ HackerRank/basic-select.sql
@@ -57,3 +57,58 @@ select city
 from station
 where lower(substring(city, -1, 1) in ('a', 'e', 'i', 'o', 'u'));
 
+/*Query the list of CITY names from STATION which have vowels 
+(i.e., a, e, i, o, and u) as both their first and last characters. 
+Your result cannot contain duplicates.*/
+select distinct city
+from station 
+where lower(substring(city, 1, 1) in ('a', 'e', 'i', 'o', 'u'))
+    AND lower(substring(city, -1, 1) in ('a', 'e', 'i', 'o', 'u'));
+
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.*/
+select distinct city
+from station 
+where lower(substr(city, 1, 1) not in ('a', 'e', 'i', 'o', 'u'));
+
+/*Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.*/
+select distinct city
+from station 
+where lower(substr(city, -1, 1) not in ('a', 'e', 'i', 'o', 'u'));
+
+/*Query the list of CITY names from STATION that either do not 
+start with vowels or do not end with vowels. Your result cannot contain duplicates.*/
+select distinct city 
+from station 
+where lower(substr(city, 1, 1) not in ('a', 'e', 'i', 'o', 'u'))
+    or lower(substr(city, -1, 1) not in ('a', 'e', 'i', 'o', 'u'));
+
+/*Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.*/
+select distinct city 
+from station 
+where lower(substr(city, 1, 1) not in ('a', 'e', 'i', 'o', 'u'))
+    and lower(substr(city, -1, 1) not in ('a', 'e', 'i', 'o', 'u'));
+
+/*Query the Name of any student in STUDENTS who scored higher than 75 Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters 
+(i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.*/
+select name
+from students
+where marks > 75
+order by substr(name, length(name) - 2, 3) asc, id asc;
+
+/*Write a query that prints a list of employee names (i.e.: the name attribute) 
+from the Employee table in alphabetical order.*/
+select name 
+from employee
+order by name asc;
+
+/*Write a query that prints a list of employee names (i.e.: the name attribute) 
+for employees in Employee having a salary greater than $2000
+per month who have been employees for less than 10 months. 
+Sort your result by ascending employee_id.*/
+select name
+from employee
+where salary > 2000 and months < 10
+order by employee_id asc;
