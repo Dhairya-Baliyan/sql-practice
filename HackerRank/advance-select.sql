29a8a93045155e63ace60824594015c18d5fc3ce
Dhairya Baliyan <dhairyabaliyan0812@gmail.com>
2026-07-16 19:50:40 +0530

    Finished Advance Select queries

--- HackerRank/advance-select.sql
+++ HackerRank/advance-select.sql
@@ -40,3 +40,56 @@ from (
     from Occupations
 ) as new_table
 group by RowNum;
+
+
+
+
+
+
+
+/*You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+*/
+select n, 
+       case 
+        when p is null then 'Root'
+        when n in (select distinct p from bst where p is not null) then 'Inner'
+        else 'Leaf'
+       end
+from bst
+order by n;
+
+
+
+
+
+
+/*Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, 
+total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+
+Note:
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. For example, 
+if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.*/
+select 
+    c.company_code,
+    c.founder,
+    count(distinct lm.lead_manager_code),
+    count(distinct sm.senior_manager_code),
+    count(distinct m.manager_code),
+    count(distinct e.employee_code)
+from company c
+left join  lead_manager lm
+    on c.company_code = lm.company_code
+left join  senior_manager sm
+    on c.company_code = sm.company_code
+left join  manager m
+    on c.company_code = m.company_code
+left join  employee e
+    on c.company_code = e.company_code
+group by c.company_code, c.founder
+order by c.company_code asc;
