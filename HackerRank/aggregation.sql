1dfb73534ac62b612ae5525045eccf0c680a75e4
Dhairya Baliyan <dhairyabaliyan0812@gmail.com>
2026-07-18 20:05:27 +0530

    Solved all easy level Aggregations problems on HackerRank

--- HackerRank/aggregation.sql
+++ HackerRank/aggregation.sql
@@ -27,4 +27,71 @@ SELECT
 FROM city;
 
 
+/*Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  
+key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (
+using salaries with any zeros removed), 
+and the actual average salary.
 
+Write a query calculating the amount of error (i.e.:actual - miscalculated average monthly salaries), and round it up to the next integer.*/
+SELECT 
+    CEIL(AVG(salary) - AVG(REPLACE(salary, '0', '')))
+FROM employees;
+
+
+/*We define an employee's total earnings to be their monthly salary x months  worked, 
+and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+Then print these values as 2 space-separated integers.*/
+SELECT 
+    months * salary as earnings,
+    COUNT(employee_id)
+FROM employee
+GROUP BY earnings
+ORDER BY earnings desc
+LIMIT 1;
+
+
+/*Query the following two values from the STATION table:
+
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all va
+*/
+SELECT 
+    ROUND(SUM(lat_n), 2) as lat,
+    ROUND(SUM(long_w), 2) as lon
+FROM station;
+
+
+/*Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 135.2345. 
+Truncate your answer to 4 decimal places.*/
+SELECT 
+    TRUNCATE(SUM(lat_n), 4) as lat_sum
+FROM station
+WHERE lat_n BETWEEN 38.7880 AND 135.2345;
+
+
+/*Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. 
+Round your answer to 4 decimal places.*/
+SELECT 
+    ROUND(long_w, 4) as western_longitude
+FROM station
+WHERE lat_n < 137.2345
+ORDER BY lat_n DESC
+LIMIT 1;
+
+
+/*Query the smallest Northern Latitude (LAT_N) from STATION that is greater than . Round your answer to 38.7780 decimal places.*/
+SELECT 
+    ROUND(MIN(lat_n), 4)
+FROM station
+WHERE lat_n > 38.7780;
+
+
+/*Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. 
+Round your answer to 4 decimal places.*/
+SELECT 
+    ROUND(long_w, 4) as western_longitude
+FROM station
+WHERE lat_n > 38.7780
+ORDER BY lat_n ASC
+LIMIT 1;
