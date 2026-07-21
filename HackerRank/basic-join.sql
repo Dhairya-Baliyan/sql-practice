2d6ba9f81debb99a006ef7edb4e5da1d8c9c597b
Dhairya Baliyan <dhairyabaliyan0812@gmail.com>
2026-07-21 19:06:57 +0530

    Solved Challeneges query on HackerRank

--- HackerRank/basic-join.sql
+++ HackerRank/basic-join.sql
@@ -98,4 +98,29 @@ ORDER BY power DESC,
 
 
 
-/**/
+/*Julia asked her students to create some coding challenges. 
+Write a query to print the hacker_id, name, and the total number of challenges created by each student. 
+Sort your results by the total number of challenges in descending order. 
+If more than one student created the same number of challenges, 
+then sort the result by hacker_id. I
+f more than one student created the same number of challenges and the count is less than the maximum number of challenges created, 
+then exclude those students from the result.*/
+WITH hacker_challenges AS(
+    SELECT h.hacker_id,
+           h.name,
+           COUNT(c.challenge_id) AS total_challenges
+    FROM hackers AS h
+    JOIN challenges AS c
+        ON h.hacker_id = c.hacker_id
+    GROUP BY h.hacker_id, h.name
+)
+
+SELECT hacker_id,
+       name,
+       total_challenges
+FROM hacker_challenges
+WHERE total_challenges = (SELECT MAX(total_challenges) FROM hacker_challenges)
+                                    OR
+      total_challenges IN 
+      (SELECT total_challenges FROM hacker_challenges GROUP BY total_challenges HAVING COUNT(*) = 1)
+ORDER BY total_challenges DESC, hacker_id ASC;
