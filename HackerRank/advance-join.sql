d92c193b764b46987f43a9d9ad81a04fd0c72755
Dhairya Baliyan <dhairyabaliyan0812@gmail.com>
2026-07-23 20:11:54 +0530

    Solved Symmetric Pairs query on HackerRank

--- HackerRank/advance-join.sql
+++ HackerRank/advance-join.sql
@@ -14,3 +14,22 @@ JOIN packages AS p2
     ON f.friend_id = p2.id
 WHERE p2.salary > p1.salary
 ORDER BY p2.salary ASC;
+
+
+
+
+/*You are given a table, Functions, containing two columns: X and Y.
+Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.
+Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.*/
+SELECT f1.x, 
+       f1.y
+FROM functions AS f1
+JOIN functions AS f2
+    ON f1.x = f2.y AND f2.x = f1.y
+/*We needed to check if f1.x = f1.y or if they are not equal and use aggregate function COUNT()*/
+GROUP BY f1.x, f1.y
+/*Case 1: if f1.x != f1.y*/
+/*Case 2: If f1.x = f1.y then the count should be greater than 1 */
+HAVING f1.x < f1.y OR (f1.x = f1.y AND COUNt(*) > 1)
+ORDER BY f1.x ASC;
+
